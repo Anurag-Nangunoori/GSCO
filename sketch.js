2ee0fcd672a096e3686be0f467b1aca13b9c8fd8
Anurag-Nangunoori <82007473+Anurag-Nangunoori@users.noreply.github.com>
2021-04-19 20:38:42 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var car,wall;
+var speed, weight;
+
+function preload() {
+
+}
+
+function setup() {
+  createCanvas(1600,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background("lightgreen");  
+  drawSprites();
+}
